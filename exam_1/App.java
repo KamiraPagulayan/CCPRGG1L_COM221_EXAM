f1ebc8acbd98a1f7b1cc7d98dfa3b0b0a0b398f1
Kalli <111488406+KamiraPagulayan@users.noreply.github.com>
2022-09-08 14:24:29 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student sheesh= new Student();
+
+        sheesh.surname = "Pagulayan";
+        sheesh.firstName = "Kamira Allison";
+        sheesh.middleInitial = 'F';
+        sheesh.dateOfBirth = 06/11/03;
+        sheesh.studentNumber = 2022103392;
+        sheesh.studentEmailAddress = "pagulayankf@students.national-u.edu.ph";
+        sheesh.iAmAwesome = true;
+        sheesh.sayMyStudentNumber();
+        sheesh.sayMyEmailAddress();
+        sheesh.amIAwesome();
+    }
+}
+
+
+
+
+
+
